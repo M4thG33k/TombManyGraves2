c1c022b05e3a295a501411da4440429ba500c4c5
Kitten <InsomniaKitten@users.noreply.github.com>
2018-04-29 23:16:48 +0100

    Replace ListIterator loop with removeIf call

--- src/main/java/com/m4thg33k/tombmanygraves/events/CommonEvents.java
+++ src/main/java/com/m4thg33k/tombmanygraves/events/CommonEvents.java
@@ -290,17 +290,7 @@ public class CommonEvents {
             return;
         }
 
-        ListIterator<EntityItem> iter = event.getDrops().listIterator();
-
-        // Remove any lists from the inventory upon death
-        while (iter.hasNext()){
-            EntityItem ei = iter.next();
-            ItemStack item = ei.getItem();
-
-            if (item.getItem() == ModItems.itemDeathList){
-                iter.remove();
-            }
-        }
+        event.getDrops().removeIf(ei -> ei.getItem().getItem() == ModItems.itemDeathList);
 
         // Get the death list from the current death
         ItemStack newList = DeathInventoryHandler.getDeathListFromFile(event.getEntityPlayer().getName(), "latest");
